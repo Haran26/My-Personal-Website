e2a7602af7d9fc6130e57c43792e068f8a909369
Haran26 <56798706+Haran26@users.noreply.github.com>
2022-05-09 19:30:30 -0400

    Changed the theme from light to dark

--- main.js
+++ main.js
@@ -25,3 +25,23 @@ window.addEventListener("click", function (e) {
     heroElement.classList.remove("is-open");
   }
 });
+
+var tabButtons = document.querySelectorAll(
+  ".tabContainer .buttonContainer button"
+);
+var tabPanels = document.querySelectorAll(".tabContainer  .tabPanel");
+
+function showPanel(panelIndex, colorCode) {
+  tabButtons.forEach(function (node) {
+    node.style.backgroundColor = "";
+    node.style.color = "";
+  });
+  tabButtons[panelIndex].style.backgroundColor = colorCode;
+  tabButtons[panelIndex].style.color = "#f4f4f4";
+  tabPanels.forEach(function (node) {
+    node.style.display = "none";
+  });
+  tabPanels[panelIndex].style.display = "block";
+  tabPanels[panelIndex].style.backgroundColor = colorCode;
+}
+showPanel(0, "#2f2f2f");
